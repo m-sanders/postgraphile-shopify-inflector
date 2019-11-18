5ad674dae6bbb21f6b726c7534e6935380234111
Martin Sanders <martin@paws.com>
2019-11-18 20:36:13 +0000

    chore: using docker-compose for local testing

--- test.sh
+++ test.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 set -e -x
-dropdb pg_simplify_inflectors || true
-createdb pg_simplify_inflectors
-psql pg_simplify_inflectors -f test.sql
-npx postgraphile -c pg_simplify_inflectors -s app_public --simple-collections both -X --export-schema-graphql ./schema.graphql
-npx postgraphile -c pg_simplify_inflectors -s app_public --simple-collections both -X --append-plugins "`pwd`/index.js" --export-schema-graphql ./schema.simplified.graphql
+
+docker-compose start
+
+DATABASE_URL=postgres://root:postgres@localhost:5432/pg_shopify_inflectors
+
+docker-compose exec db dropdb pg_shopify_inflectors || true
+docker-compose exec db createdb pg_shopify_inflectors
+docker-compose exec db psql pg_shopify_inflectors -f test.sql
+npx postgraphile -c ${DATABASE_URL} -s app_public --simple-collections both -X --export-schema-graphql ./schema.graphql
+npx postgraphile -c ${DATABASE_URL} -s app_public --simple-collections both -X --append-plugins "`pwd`/index.js" --export-schema-graphql ./schema.shopify.graphql
 if command -v colordiff; then
   COLORDIFF="colordiff"
 else
   COLORDIFF="cat"
 fi;
-diff -u ./schema.graphql ./schema.simplified.graphql | tee schema.graphql.diff | $COLORDIFF
+diff -u ./schema.graphql ./schema.shopify.graphql | tee schema.graphql.diff | $COLORDIFF
